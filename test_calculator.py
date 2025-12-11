c1a2c53777a36d6077673e2fd045d2ddf4c77622
Vladimir <vova.kotje@gmail.ru>
2025-12-11 22:01:54 +0500

    Тестирование пайплайна №1

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ from calculator import add, subtract, multiply, divide
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 6)
         self.assertEqual(add(-1, 1), 0)
     
     def test_subtract(self):
